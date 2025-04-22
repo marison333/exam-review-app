2cfbc4df12f2a7ab015639b3b0d009f6c9a97b1a
marison333 <marison.sol@outlook.com>
2025-04-22 15:53:43 +0200

    created api-endpoint

---
+++ src/app/api/lambda/route.ts
@@ -0,0 +1,35 @@
+type HomeProps = {
+    buttonLabel: string;
+    buttonTarget: string;
+    buttonUrl: string;
+    imageAlt: string;
+    imageUrl: string;
+    title: string;
+    subTitle: string;
+};
+
+export async function GET() {
+    const response = await fetch('https://47c69rsiqh.execute-api.eu-central-1.amazonaws.com/marison_lambda_function')
+    if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const res = await response.json();
+    
+    const homeData: HomeProps = {
+        buttonLabel: res.buttonLabel,
+        buttonTarget: res.buttonTarget,
+        buttonUrl: res.buttonUrl,
+        imageAlt: res.imageAlt,
+        imageUrl: res.imageUrl,
+        title: res.title,
+        subTitle: res.subTitle
+    };
+
+    return new Response(JSON.stringify(homeData), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+}
